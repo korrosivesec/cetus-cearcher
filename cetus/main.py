fa5e0fac22e8427d83a2475061018da69584f8c5
Kory Kyzar <kory.kyzar@lumen.com>
2021-06-10 17:37:16 -0400

    edit pyproject.toml

--- cetus/main.py
+++ cetus/main.py
@@ -1,19 +1,20 @@
-
 import os
+from enum import Enum
+
 import typer
 from rich.console import Console
 from rich.syntax import Syntax
 from rich.traceback import install
-from enum import Enum
+
 from . import search
 
 # Initiate Rich traceback handlers
 install()
 
 console = Console()
-error_console = Console(stderr=True, style='bold red')
+error_console = Console(stderr=True, style="bold red")
 
-app = typer.Typer(name='cetus', help="Cetus Cearcher")
+app = typer.Typer(name="cetus", help="Cetus Cearcher")
 app.add_typer(search.app, name="search", help="Search Cetus")
 
 
@@ -23,9 +24,12 @@ class index(str, Enum):
 
 
 @app.command(help="Show example result data")
-def example(index: index = typer.Argument(..., case_sensitive=False),
-            raw: bool = typer.Option(False, "--raw", help="Print raw output instead of pretty printed")
-            ):
+def example(
+    index: index = typer.Argument(..., case_sensitive=False),
+    raw: bool = typer.Option(
+        False, "--raw", help="Print raw output instead of pretty printed"
+    ),
+):
 
     path = os.path.dirname(os.path.abspath(__file__))
     file = f"{path}/examples/{index.value}.json"
