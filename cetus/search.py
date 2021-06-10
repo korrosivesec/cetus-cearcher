9570892d9392f911ae7a8bd3e5aa95f643d7c953
Kory Kyzar <kory.kyzar@lumen.com>
2021-06-10 17:00:55 -0400

    update deps.  change api_url

--- cetus/search.py
+++ cetus/search.py
@@ -1,19 +1,20 @@
-
 import json
+from datetime import datetime
+
 import requests
 import typer
-from datetime import datetime
 from rich.console import Console
 from rich.syntax import Syntax
 from rich.traceback import install
-from .config import api_url, api_key
+
+from .config import api_key, api_url
 
 # Initiate Rich traceback handlers
 install()
 
 app = typer.Typer()
 console = Console()
-error_console = Console(stderr=True, style='bold red')
+error_console = Console(stderr=True, style="bold red")
 
 now = datetime.now().replace(microsecond=0).isoformat()
 
@@ -23,13 +24,18 @@ def process_query(index, query, start, end, raw, metadata):
     end = end.replace(microsecond=0).isoformat()
     results = None
 
-    if index == 'DNS':
-        query_string = f'host.raw:{query} AND collection_timestamp:[{start} TO {end}]&index=dns'
+    if index == "DNS":
+        query_string = (
+            f"host.raw:{query} AND collection_timestamp:[{start} TO {end}]&index=dns"
+        )
     else:
-        query_string = f'data.leaf_cert.all_domains:{query} AND collection_timestamp:[{start} TO {end}]&index=certstream'
+        query_string = f"data.leaf_cert.all_domains:{query} AND collection_timestamp:[{start} TO {end}]&index=certstream"
 
-    with console.status(f'[bold green]Searching Cetus {index} index...'):
-        results = requests.get(f'{api_url}{query_string}', headers={'Authorization': f'Token {api_key}'})
+    with console.status(f"[bold green]Searching Cetus {index} index..."):
+        results = requests.get(
+            f"{api_url}/api/query?q={query_string}",
+            headers={"Authorization": f"Token {api_key}"},
+        )
         try:
             # Validates JSON response and convert to Python dict
             data = json.loads(results.text)
@@ -39,15 +45,15 @@ def process_query(index, query, start, end, raw, metadata):
             return
 
     if not metadata:
-        data = data['data']['es_response']
+        data = data["data"]["es_response"]
         if not data:
             if not raw:
-                console.print(':pile_of_poo: No results found.')
+                console.print(":pile_of_poo: No results found.")
             else:
                 print(json.dumps(data))
             return
 
-    formatted_data = Syntax(json.dumps(data, indent=4), 'json')
+    formatted_data = Syntax(json.dumps(data, indent=4), "json")
     console.print(formatted_data) if not raw else print(json.dumps(data))
 
 
@@ -55,25 +61,37 @@ def process_query(index, query, start, end, raw, metadata):
 def dns(
     query: str = typer.Argument(..., help="Lucene formatted query."),
     start: datetime = typer.Option(..., help="Beginning of search range. Required."),
-    end: datetime = typer.Option(str(now), help="End of search range. If omitted, end == now"),
-    raw: bool = typer.Option(False, "--raw", help="Print raw output instead of pretty printed"),
-    metadata: bool = typer.Option(False, "--metadata", help="Include metadata in output")
+    end: datetime = typer.Option(
+        str(now), help="End of search range. If omitted, end == now"
+    ),
+    raw: bool = typer.Option(
+        False, "--raw", help="Print raw output instead of pretty printed"
+    ),
+    metadata: bool = typer.Option(
+        False, "--metadata", help="Include metadata in output"
+    ),
 ):
     """
     Search the Cetus DNS index
     """
-    process_query('DNS', query, start, end, raw, metadata)
+    process_query("DNS", query, start, end, raw, metadata)
 
 
 @app.command()
 def certstream(
     query: str = typer.Argument(..., help="Lucene formatted query."),
     start: datetime = typer.Option(..., help="Beginning of search range. Required."),
-    end: datetime = typer.Option(str(now), help="End of search range. If omitted, end == now"),
-    raw: bool = typer.Option(False, "--raw", help="Print raw output instead of pretty printed"),
-    metadata: bool = typer.Option(False, "--metadata", help="Include metadata in output")
+    end: datetime = typer.Option(
+        str(now), help="End of search range. If omitted, end == now"
+    ),
+    raw: bool = typer.Option(
+        False, "--raw", help="Print raw output instead of pretty printed"
+    ),
+    metadata: bool = typer.Option(
+        False, "--metadata", help="Include metadata in output"
+    ),
 ):
     """
     Search the Cetus Certstream index
     """
-    process_query('Certstream', query, start, end, raw, metadata)
+    process_query("Certstream", query, start, end, raw, metadata)
